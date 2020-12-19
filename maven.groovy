3384b14073efb8411cbdb21247accb22c83503e9
juansalinasaedo <juan.salinas.aedo@gmail.com>
2020-12-19 17:12:31 -0300

    ejecucion taller 3 - actualizacion 6 19-12-20

--- maven.groovy
+++ maven.groovy
@@ -1,41 +1,34 @@
 def call() {
+  
+    stage('Compile'){
+		STAGE_NAME = 'Compile'
+		sh 'mvn clean compile -e'
+	}
 
-	stage('Compile') {
-        env.TAREA = env.STAGE_NAME
-        git( branch: 'main', url: 'https://github.com/juansalinasaedo/ejemplo-gradle.git')
-        sh './mvnw clean compile -e'
-    }
-
-    stage('Test') {
-        env.TAREA = env.STAGE_NAME
-        sh './mvnw test -e'
-    }
-
-    stage('Jar') {
-        env.TAREA = env.STAGE_NAME
-        sh './mvnw package -e -DskipTests=true'
-    }
+	stage('Test'){
+		STAGE_NAME  = 'Test'
+		sh 'mvn clean test -e'
+	}
 
 
-    stage('Run') {
-        env.TAREA = env.STAGE_NAME
-        sh './mvnw spring-boot:run &'
-    }
-
-    
-    stage('SonarQube') {
-        env.TAREA = env.STAGE_NAME
-        withSonarQubeEnv(installationName: 'sonar') {
-            sh './mvnw org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
-        }
-    }
+    stage('Jar'){
+		STAGE_NAME = Jar
+		sh 'mvn clean package -e'
+	}
 
 
-     /* stage('uploadNexus') {
+    stage('SonarQube'){
+		STAGE_NAME = 'SonarQube'
+		withSonarQubeEnv(installationName: 'sonar'){
+			sh 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+		}
+	}
+
+      stage('uploadNexus') {
          STAGE_NAME = 'uploadNexus'
          nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
          
-       }   */
+       }   
 }
 
 return this;
